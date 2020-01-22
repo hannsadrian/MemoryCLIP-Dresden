07969ae07ab693c2564f9e539506fece26fb6c1e
Adwirawien <hannsadrian@gmail.com>
2020-01-22 21:11:03 +0100

    Make mobile friendly

--- website/src/components/Home.js
+++ website/src/components/Home.js
@@ -61,26 +61,30 @@ class Home extends Component {
     render() {
         return (
             <div className="min-h-screen bg-gray-100 pt-10 px-6 sm:pl-32 sm:pt-20">
-                <h1 className="text-2xl font-medium select-none">📚 Memory<span className="font-bold">CLIP</span></h1>
+                <h1 className="text-2xl font-medium select-none"><span role="img"
+                                                                       aria-label="books">📚</span> Memory<span
+                    className="font-bold">CLIP</span></h1>
                 <p className="text-gray-900">Dieses Projekt bietet eine frei wählbare Erinnerung<br/>
                     an die Bombardierung Dresdens.</p>
                 <input onChange={this.search.bind(this)} placeholder="Ort suchen"
                        className="mt-4 mb-2 p-2 px-3 max-w-md bg-gray-100 rounded w-full border-1pt border-gray-400 shadow-lg outline-none focus:outline-none focus:border-blue-600 trans"/>
                 <div className="pb-6 max-w-md">
                     {this.state.results.map((val, index) => (
-                        <Link to={"/article/"+val.id}>
-                            <div key={index}
-                                 className="select-none flex my-2 pl-1 p-2 bg-gray-100 border-gray-400 rounded cursor-pointer border-1px shadow hover:shadow-md trans">
-                                <div className="w-10 h-10 my-auto mx-2 rounded-full"
-                                     style={{backgroundImage: 'url(' + val.img[0] + ')', backgroundSize: 'cover'}}
-                                />
-                                <div className="w-9/12">
-                                    <h2 className="text-lg font-semibold">{val.name}</h2>
-                                    <p className="text-sm truncate">{val.article}</p>
-                                    <Label type={val.type}/>
+                        <div className="my-2 rounded">
+                            <Link to={"/article/" + val.id}>
+                                <div key={index}
+                                     className="select-none flex pl-1 p-2 bg-gray-100 border-gray-400 cursor-pointer rounded border-1px shadow hover:shadow-md trans">
+                                    <div className="w-10 h-10 my-auto mx-2 rounded-full"
+                                         style={{backgroundImage: 'url(' + val.img[0] + ')', backgroundSize: 'cover'}}
+                                    />
+                                    <div className="w-9/12">
+                                        <h2 className="text-lg font-semibold">{val.name}</h2>
+                                        <p className="text-sm truncate">{val.article}</p>
+                                        <Label type={val.type}/>
+                                    </div>
                                 </div>
-                            </div>
-                        </Link>
+                            </Link>
+                        </div>
                     ))}
                 </div>
             </div>
